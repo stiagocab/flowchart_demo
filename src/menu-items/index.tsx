4aac7683b8e3bb5e4e7708725591a9fdda2d1238
Santiago Cabrera <45210638+stiagocab@users.noreply.github.com>
2023-06-26 20:15:41 -0600

    Remove unuse pages

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -1,12 +1,5 @@
 import dashboard from './dashboard';
-// import widget from './widget';
-// import application from './application';
-// import forms from './forms';
-// import elements from './elements';
-// import pages from './pages';
-// import utilities from './utilities';
-// import support from './support';
-// import other from './other';
+
 import { NavItemType } from 'types';
 
 // ==============================|| MENU ITEMS ||============================== //
